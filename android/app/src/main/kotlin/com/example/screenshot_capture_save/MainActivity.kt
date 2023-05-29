fb87d5bf22e2f96df15464ec47274609481633bf
Nahianether <nahianether3@gmail.com>
2023-05-29 17:34:35 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/screenshot_capture_save/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.screenshot_capture_save
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          headline6: TextStyle(
+            color: Colors.yellow,
+          ),
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Screenshot Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  ScreenshotController screenshotController = ScreenshotController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Screenshot(
+              controller: screenshotController,
+              child: Container(
+                padding: const EdgeInsets.all(30.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent, width: 5.0),
+                  color: Colors.amberAccent,
+                ),
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/flutter_logo.png',
+                    ),
+                    const Text("This widget will be captured as an image"),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture Above Widget',
+              ),
+              onPressed: () {
+                screenshotController
+                    .capture(delay: const Duration(milliseconds: 10))
+                    .then((capturedImage) async {
+                  showCapturedWidget(context, capturedImage!);
+                }).catchError((onError) {
+                  log(onError);
+                });
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture An Invisible Widget',
+              ),
+              onPressed: () {
+                var container = Container(
+                    padding: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent, width: 5.0),
+                      color: Colors.redAccent,
+                    ),
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          'assets/flutter_logo.png',
+                        ),
+                        Text(
+                          "This is an invisible widget",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ],
+                    ));
+                screenshotController
+                    .captureFromWidget(
+                  InheritedTheme.captureAll(
+                    context,
+                    Material(child: container),
+                  ),
+                  delay: const Duration(seconds: 1),
+                )
+                    .then((capturedImage) {
+                  showCapturedWidget(context, capturedImage);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> showCapturedWidget(
+      BuildContext context, Uint8List capturedImage) {
+    return showDialog(
+      useSafeArea: false,
+      context: context,
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Captured widget screenshot"),
+        ),
+        body: Center(
+          child:
+              capturedImage != null ? Image.memory(capturedImage) : Container(),
+        ),
+      ),
+    );
+  }
+}
